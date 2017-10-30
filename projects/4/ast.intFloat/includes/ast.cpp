29f9a4a7c2cf9787f0f82f1888cae4742cfa5d09
Brian Malloy <malloy@clemson.edu>
2017-10-30 11:35:22 -0400

    new changes

---
+++ projects/4/ast.intFloat/includes/ast.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <typeinfo>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <iomanip>
+#include "ast.h"
+#include "symbolTable.h"
+
+void freeAST(Node* node) {
+  if ( node ) {
+    BinaryNode* temp = dynamic_cast<BinaryNode*>(node);
+    if ( temp ) {
+      freeAST(temp->getLeft());
+      freeAST(temp->getRight());
+    }
+    delete node;
+  }
+}
+
+const Literal* IdentNode::eval() const { 
+  const Literal* val = SymbolTable::getInstance().getValue(ident);
+  return val;
+}
+
+
+AsgBinaryNode::AsgBinaryNode(Node* left, Node* right) : 
+  BinaryNode(left, right) { 
+  const Literal* res = right->eval();
+  const std::string n = static_cast<IdentNode*>(left)->getIdent();
+  SymbolTable::getInstance().setValue(n, res);
+}
+
+
+const Literal* AsgBinaryNode::eval() const { 
+  if (!left || !right) {
+    throw "error";
+  }
+  const Literal* res = right->eval();
+
+  const std::string n = static_cast<IdentNode*>(left)->getIdent();
+  SymbolTable::getInstance().setValue(n, res);
+  return res;
+}
+
+const Literal* AddBinaryNode::eval() const { 
+  if (!left || !right) {
+    throw "error";
+  }
+  const Literal* x = left->eval();
+  const Literal* y = right->eval();
+  return (*x+*y);
+}
+
+const Literal* SubBinaryNode::eval() const { 
+  if (!left || !right) {
+    throw "error";
+  }
+  const Literal* x = left->eval();
+  const Literal* y = right->eval();
+  return ((*x)-(*y));
+}
+
+const Literal* MulBinaryNode::eval() const { 
+  if (!left || !right) {
+    throw "error";
+  }
+  const Literal* x = left->eval();
+  const Literal* y = right->eval();
+  return ((*x)*(*y));
+}
+
+const Literal* DivBinaryNode::eval() const { 
+  if (!left || !right) {
+    throw "error";
+  }
+  const Literal* x = left->eval();
+  const Literal* y = right->eval();
+  if ( y->eval() == 0 ) {
+    throw std::string("ZeroDivisionError");
+  }
+  return ((*x)/(*y));
+}
+
